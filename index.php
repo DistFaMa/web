4404371cbcac8ed244e2484392b0ef2f1f4fb517
DistFaMa <161954058+DistFaMa@users.noreply.github.com>
2024-03-02 12:05:01 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,253 @@
+﻿
+<!DOCTYPE html><!-- HTML5 -->
+<html prefix="og: http://ogp.me/ns#" lang="es-ES" dir="ltr">
+	<head>
+		<title>AccesoriosFA-MA</title>
+		<meta charset="utf-8" />
+		<!--[if IE]><meta http-equiv="ImageToolbar" content="False" /><![endif]-->
+		<meta name="author" content="Gordillo Mauro" />
+		<meta name="generator" content="Incomedia WebSite X5 Professional 13.0.1.16 - www.websitex5.com" />
+		<meta name="description" content="Distribuidora De Accesorios Para Automoviles" />
+		<meta property="og:locale" content="es" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="http://www.accesoriosfa-ma.com/index.php" />
+		<meta property="og:title" content="Novedades" />
+		<meta property="og:site_name" content="AccesoriosFA-MA" />
+		<meta property="og:image" content="http://www.accesoriosfa-ma.com/favImage.png" />
+		<meta property="og:image:type" content="image/png">
+		<meta property="og:image:width" content="1011">
+		<meta property="og:image:height" content="461">
+		<meta name="viewport" content="width=1024" />
+		
+		<link rel="stylesheet" type="text/css" href="style/reset.css?13-0-1-16" media="screen,print" />
+		<link rel="stylesheet" type="text/css" href="style/print.css?13-0-1-16" media="print" />
+		<link rel="stylesheet" type="text/css" href="style/style.css?13-0-1-16" media="screen,print" />
+		<link rel="stylesheet" type="text/css" href="style/template.css?13-0-1-16" media="screen" />
+		<link rel="stylesheet" type="text/css" href="style/menu.css?13-0-1-16" media="screen" />
+		<link rel="stylesheet" type="text/css" href="pcss/index.css?13-0-1-16-636276118599552186" media="screen" />
+		<script type="text/javascript" src="res/jquery.js?13-0-1-16"></script>
+		<script type="text/javascript" src="res/x5engine.js?13-0-1-16" data-files-version="13-0-1-16"></script>
+		
+		
+		
+		<script type="text/javascript">
+			window.onload = function(){ checkBrowserCompatibility('El Explorador que estás usando no es compatible con las funciones requeridas para mostrar este Sitio web.','El Navegador que estás utilizando podría no ser compatible con las funciones requeridas para poder ver este Sitio web.','[1]Actualiza tu explorador [/1] o [2]continuar de todos modos[/2].','http://outdatedbrowser.com/'); };
+		</script>
+		<link rel="stylesheet" type="text/css" href="engine1/style.css" />
+	<script type="text/javascript" src="engine1/jquery.js"></script>
+	</head>
+<body>
+
+
+		<div id="imHeaderBg"></div>
+		<div id="imFooterBg"></div>
+		<div id="imPage">
+			<div id="imHeader">
+				<h1 class="imHidden">AccesoriosFA-MA</h1>
+				<div id="imHeaderObjects"><div id="imHeader_imTextObject_02_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_2" class="" > <div id="imHeader_imCellStyleGraphics_2"></div><div id="imHeader_imCellStyle_2" ><div id="imHeader_imTextObject_02">
+	<div class="text-tab-content"  id="imHeader_imTextObject_02_tab0" style="">
+		<div class="text-inner">
+			<div><span class="fs8">Alvear 1496</span></div><div><span class="fs8">Rosario, Santa Fe</span></div><div><span class="fs8">Argentina</span></div><div><br></div><div><span class="fs8">(0341) 155-763058 (</span><span class="fs8">Fabian)</span><br></div><div><span class="fs8"><span style="line-height: 17px;">(0341) 155-709722 (</span>Matias)</span><br></div><div><div><span class="fs8"><img src="http://pluspng.com/img-png/whatsapp-png-white-whatsapp-icon-256.png" width="25" height="25" border="0"> (0341) 448-2443 <img src="http://pluspng.com/img-png/whatsapp-png-white-whatsapp-icon-256.png" width="25" height="25" border="0"> (Whatsapp Negocio)</span></div></div><div><br></div><div><span class="fs8">accesoriosfamaok@gmail.com (Deposito)</span><br></div>
+		</div>
+	</div>
+
+</div>
+</div></div></div><div id="imHeader_imObjectImage_03_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_3" class="" > <div id="imHeader_imCellStyleGraphics_3"></div><div id="imHeader_imCellStyle_3" ><img id="imHeader_imObjectImage_03" src="images/white_no.circle_phone.png" title="" alt="" /></div></div></div><div id="imHeader_imObjectImage_04_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_4" class="" > <div id="imHeader_imCellStyleGraphics_4"></div><div id="imHeader_imCellStyle_4" ><img id="imHeader_imObjectImage_04" src="images/white_no.circle_mail.png" title="" alt="" /></div></div></div><div id="imHeader_imObjectImage_05_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_5" class="" > <div id="imHeader_imCellStyleGraphics_5"></div><div id="imHeader_imCellStyle_5" ><img id="imHeader_imObjectImage_05" src="images/white_no.circle_position.png" title="" alt="" /></div></div></div><div id="imHeader_imObjectImage_06_wrapper" class="template-object-wrapper"><div id="imHeader_imCell_6" class="" > <div id="imHeader_imCellStyleGraphics_6"></div><div id="imHeader_imCellStyle_6" ><img id="imHeader_imObjectImage_06" src="images/logo.gif" title="" alt="" /></div></div></div></div>
+			</div>
+			<a class="imHidden" href="#imGoToCont" title="Salta el menu principal">Vaya al Contenido</a>
+			<a id="imGoToMenu"></a><p class="imHidden">Menu Principal:</p>
+			<div id="imMnMnContainer">
+				<div id="imMnMnGraphics"></div>
+				<div id="imMnMn" class="auto main-menu">
+					<div class="hamburger-menu-background-container"><div class="hamburger-menu-background menu-mobile-hidden"><div class="hamburger-menu-close-button"><span>&times;</span></div></div></div>
+				<ul class="auto menu-mobile-hidden">
+					<li id="imMnMnNode0" class="imPage imMnMnCurrent">
+						<a href="index.php">
+							<span class="imMnMnFirstBg">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">Novedades</span></span>
+							</span>
+						</a>
+					</li><li id="imMnMnNode7" class=" imLevel">
+						<span class="imMnMnFirstBg">
+							<span class="imMnMnLevelImg"></span><span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">Catalogos</span></span>
+						</span>
+				<ul class="auto">
+					<li id="imMnMnNode8" class="imMnMnFirst imPage">
+						<a href="linea-qkl---laffitte.html">
+							<span class="imMnMnBorder">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">Linea QKL - LAFFITTE</span></span>
+							</span>
+						</a>
+					</li><li id="imMnMnNode9" class="imMnMnMiddle imPage">
+						<a href="linea-lyf.html">
+							<span class="imMnMnBorder">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">Linea LYF</span></span>
+							</span>
+						</a>
+					</li><li id="imMnMnNode10" class="imMnMnMiddle imPage">
+						<a href="cubre-asiento.html">
+							<span class="imMnMnBorder">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">Cubre Asiento</span></span>
+							</span>
+						</a>
+					</li><li id="imMnMnNode11" class="imMnMnMiddle imPage">
+						<a href="volantes.html">
+							<span class="imMnMnBorder">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">Volantes</span></span>
+							</span>
+						</a>
+					</li><li id="imMnMnNode12" class="imMnMnMiddle imPage">
+						<a href="herramientas.html">
+							<span class="imMnMnBorder">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">Herramientas</span></span>
+							</span>
+						</a>
+					</li><li id="imMnMnNode13" class="imMnMnMiddle imPage">
+						<a href="fichas-y-terminales.html">
+							<span class="imMnMnBorder">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">Fichas y Terminales</span></span>
+							</span>
+						</a>
+					</li><li id="imMnMnNode14" class="imMnMnLast imPage">
+						<a href="lamparas-leds.html">
+							<span class="imMnMnBorder">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">Lamparas Leds</span></span>
+							</span>
+						</a>
+					</li></ul></li>
+				<li id="imMnMnNode6" class=" imPage">
+						<a href="lista-de-precios.html">
+							<span class="imMnMnFirstBg">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">Lista de Precios</span></span>
+							</span>
+						</a>
+					</li><li id="imMnMnNode15" class=" imPage">
+						<a href="-donde-estamos-.html">
+							<span class="imMnMnFirstBg">
+								<span class="imMnMnTxt"><span class="imMnMnImg"></span><span class="imMnMnTextLabel">¿Donde Estamos?</span></span>
+							</span>
+						</a><a class='boton' href='asistencia.html' ><img src='asistencia.png' border="0"/></a>
+
+<style>.boton {
+    display:scroll;
+        position:fixed;
+        bottom:320px;
+        right:0px;
+} </style>
+					</li></ul>
+				</div>
+			</div>
+			<div id="imContentContainer">
+				<div id="imContentGraphics"></div>
+				<div id="imContent">
+    							<div style="text-align: center;">
+							  <p align="center">VER LA GALERIA  EN PANTALLA COMPLETA							    <A HREF="http://www.accesoriosfa-ma.com/fotos/">HACER CLICK AQUI... <img src="galeria.png" width="60" height="60" />  </A></p>
+					<a id="imGoToCont"></a>
+					<div id="wowslider-container1">
+	<div class="ws_images"><ul>
+	
+
+<?php
+$contador=0;
+
+$folder_path = 'fotos/'; //image's folder path
+
+$num_files = glob($folder_path . "*.{JPG,jpg,gif,png,bmp}", GLOB_BRACE);
+
+$folder = opendir($folder_path);
+
+if($num_files > 0)
+{
+	while(false !== ($file = readdir($folder)))
+	{
+		$file_path = $folder_path.$file;
+		$extension = strtolower(pathinfo($file ,PATHINFO_EXTENSION));
+		if($extension=='jpg' || $extension =='png' || $extension == 'gif' || $extension == 'bmp')
+		{
+        $contador++;
+			?>
+            <li><img src="<?php echo $file_path; ?>">id="wows1_<?php echo $contador; ?>"/></li>
+
+            <?php
+		}
+	}
+}
+
+else
+
+
+{
+	echo "the folder was empty !";
+}
+closedir($folder);
+?>
+   	</ul></div>
+<div class="ws_bullets"><div>
+<?php
+$contador=0;
+
+$folder_path = 'fotos/'; //image's folder path
+
+$num_files = glob($folder_path . "*.{JPG,jpg,gif,png,bmp}", GLOB_BRACE);
+
+$folder = opendir($folder_path);
+
+if($num_files > 0)
+{
+	while(false !== ($file = readdir($folder)))
+	{
+		$file_path = $folder_path.$file;
+		$extension = strtolower(pathinfo($file ,PATHINFO_EXTENSION));
+		if($extension=='jpg' || $extension =='png' || $extension == 'gif' || $extension == 'bmp')
+		{
+        $contador++;
+			?>
+			<a href="#" title=""><span><?php echo $contador; ?></span></a>
+
+            <?php
+		}
+	}
+}
+
+else
+
+
+{
+	echo "the folder was empty !";
+}
+closedir($folder);
+?>
+   	</ul></div>
+
+
+
+	</div></div><div class="ws_script" style="position:absolute;left:-99%"><a href="http://wowslider.com">http://wowslider.com/</a> by WOWSlider.com v8.7</div>
+	<div class="ws_shadow"></div>
+	</div>
+	<script type="text/javascript" src="engine1/wowslider.js"></script>
+	<script type="text/javascript" src="engine1/script.js"></script>
+	<!-- End WOWSlider.com BODY section -->
+</div>
+				
+				<div id="imFooPad"></div><div id="imBtMnContainer" class="imContentDataContainer"><div id="imBtMn"><a href="index.php">Novedades</a> | <a href="linea-qkl---laffitte.html">Catalogos</a> | <a href="lista-de-precios.html">Lista de Precios</a> | <a href="-donde-estamos-.html">¿Donde Estamos?</a> | <a href="imsitemap.html">Mapa general del sitio</a></div></div>
+					<div class="imClear"></div>
+				</div>
+			</div>
+			<div id="imFooter">
+				<div id="imFooterObjects"><div id="imFooter_imTextObject_02_wrapper" class="template-object-wrapper"><div id="imFooter_imCell_2" class="" > <div id="imFooter_imCellStyleGraphics_2"></div><div id="imFooter_imCellStyle_2" ><div id="imFooter_imTextObject_02">
+	<div class="text-tab-content"  id="imFooter_imTextObject_02_tab0" style="">
+		<div class="text-inner">
+			<div style="text-align: center;"> Nos dedicamos a la Venta por mayor de accesorios para autos: </div><div style="text-align: center;">Tunning, Fundas cubre asientos, herramientas y elementos de seguridad.</div><div style="text-align: center;">Accesorios para el automotor.<br></div>
+		</div>
+	</div>
+
+</div>
+</div></div></div><div id="imFooter_imObjectSearch_01_wrapper" class="template-object-wrapper"><div id="imFooter_imCell_1" class="" > <div id="imFooter_imCellStyleGraphics_1"></div><div id="imFooter_imCellStyle_1" ><div id="imFooter_imObjectSearch_01"><form id="imFooter_imObjectSearch_01_form" action="imsearch.php" method="get"><fieldset><input type="text" id="imFooter_imObjectSearch_01_field" name="search" value="" /><button id="imFooter_imObjectSearch_01_button">Cerca</button></fieldset></form><script>$('#imFooter_imObjectSearch_01_button').click(function() { $('#imFooter_imObjectSearch_01_form').submit(); return false; });</script></div></div></div></div><div id="imFooter_imObjectImage_05_wrapper" class="template-object-wrapper"><div id="imFooter_imCell_5" class="" > <div id="imFooter_imCellStyleGraphics_5"></div><div id="imFooter_imCellStyle_5" ></div></div></div></div>
+			</div>
+		</div>
+		<span class="imHidden"><a href="#imGoToCont" title="Lea esta página de nuevo">Regreso al contenido</a> | <a href="#imGoToMenu" title="Lea este sitio de nuevo">Regreso al menu principal</a></span>
+		
+		<noscript class="imNoScript"><div class="alert alert-red">Para utilizar este sitio tienes que habilitar JavaScript.</div></noscript>
+	</body>
+</html><div style="position: absolute; top: -999px;left: -999px;">一有空我就泡在网上查产后怎样健康丰胸的方法，看到运动可以丰胸<a href="https://1807614030.wixsite.com/fngzgw" title="产后丰胸产品">产后丰胸产品</a>，我就天天做运动，坚持了一个月，可是我没发现胸部有什么变大，反而觉得劳累不堪。后来有人说食物丰胸也不错，我又开始吃一些丰胸的食物<a href="http://fngzasia.com" title="丰胸效果">丰胸效果</a>，可结果依然让我很失望。那段时间我就像着魔了一样，甚至打算去做丰胸手术，结果被老公拦了下来<a href="http://fngzweb.com" title="燕窝酒酿蛋">燕窝酒酿蛋</a>，他说胸部下垂也没关系，健康才是最重要的。看着老公，我很感动，但为了我们的夫妻生活，我一定要找到丰胸的方法<a href="https://1807614030.wixsite.com/fngzjnd" title="丰胸最快方法">丰胸最快方法</a>！</div>
+
